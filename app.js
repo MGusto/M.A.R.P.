ae589052e114137a5e909ab57045da27cde69a64
MGusto <felixbjorseth@outlook.com>
2025-04-03 16:03:23 +0200

    Removed uneccessary JS files

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ function show() {
         <h2 id="maintitle">M.A.R.P.</h2>
             <h3 id="subtitle">(Or "MGusto's Anomaly Randomizer Project")</h3>
             </div>
-            <div id="navbar"><button id="introbtn" class="navbtn">Intro</button><button id="namegenbtn" class="navbtn">Name & Faction</button><button id="hideoutbtn" class="navbtn">Hideouts</button><button id="aboutbtn" class="navbtn">About</button></div>
+            <div id="navbar"><button id="introbtn" class="navbtn" onclick="swapView(1)">Intro</button><button id="namegenbtn" class="navbtn">Name & Faction<br>Generator</button><button id="hideoutbtn" class="navbtn">Hideout Picker</button><button id="aboutbtn" class="navbtn" onclick="swapView(2)">About</button></div>
             <br>
-            <div class="contentBox">
-                <p>This is a little project I'm building relating to <a href="https://www.moddb.com/mods/stalker-anomaly" target="_blank">S.T.A.L.K.E.R. Anomaly</a> and the mod-packs that run on top of it. The intent is to make a web-based tool that can be used by players to make certain decisions for them, either for the sake of inspiration or for challenge-runs and that sort of thing.</p>
-                </div>`;
+            ${currentView}`;
     app.innerHTML = html;
+}
+
+function swapView(num) {
+    currentView = '';
+    const view = model.data.views[num - 1].htmlcont;
+    currentView = view;
+    show();
 }
---
+++ model.js
@@ -0,0 +1,42 @@
+let currentView = 1;
+const model = {
+    app: {
+    },
+    inputs: {
+
+    },
+    data: {
+        views: [
+            {id: 1, name: 'intro', htmlcont: /*HTML*/ `<div class="contentBox">
+<p>This is a little project I'm building relating to <a href="https://www.moddb.com/mods/stalker-anomaly" target="_blank">S.T.A.L.K.E.R. Anomaly</a> and the mod-packs that run on top of it. The intent is to make a web-based tool that can be used by players to make certain decisions for them, either for the sake of inspiration or for challenge-runs and that sort of thing.</p>
+</div>` },
+            {id: 2, name: 'about', htmlcont: /*HTML*/ `<div class="contentBox">
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    <p>This is a section where I put links and stuff.</p>
+    </div>`},
+        ]
+
+    }
+}
