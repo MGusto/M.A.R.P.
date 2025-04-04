101f3d79f2982772a2e1df090d8e672305cba2a5
MGusto <felixbjorseth@outlook.com>
2025-04-04 14:29:48 +0200

    Started work on Faction and Name Generator
    Added Faction icons, database entries under namegen.js, and started compiling names into the same document.

--- model.js
+++ model.js
@@ -22,15 +22,14 @@ const model = {
             {
                 id: 4, name: 'about', htmlcont: /*HTML*/ `<div class="contentBox">
     <p>This is a section where I put links and stuff.</p>
-    <ol>
+    <ul>
     <li><a href="https://github.com/MGusto/M.A.R.P." target="_blank">Project GitHub Repo</a></li>
     <li>HUH?!</li>
     <li><a href="https://www.moddb.com/mods/stalker-anomaly" target="_blank">S.T.A.L.K.E.R. Anomaly</a></li>
     <li><a href="https://github.com/Grokitach/Stalker_GAMMA" target="_blank">S.T.A.L.K.E.R. GAMMA mod-pack for Anomaly</a></li>
     <li>Original S.T.A.L.K.E.R. Trilogy <a href="https://store.steampowered.com/sub/35983/" target="_blank">on Steam</a> and <a href="www.gog.com/en/games?query=S.T.A.L.K.E.R." target="_blank">on GOG</a></li>
-</ol>
+</ul>
     </div>`},
-        ]
-
+        ],
     }
 }
