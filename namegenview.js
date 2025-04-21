ca9d11b0121c4ad38f8c0ace0a347f2b7ab9e95f
MGusto <felixbjorseth@outlook.com>
2025-04-21 20:14:42 +0200

    Added all factions into namegenview
    Now descriptions and enemy lists can be viewed for every faction.

--- namegenview.js
+++ namegenview.js
@@ -5,8 +5,18 @@ function nameGenView() {
     <h3>Choose the faction you want to play!</h3>
     <div id="namefaction"></div>
     <div id="factionbox">
-    <div class ="factionchoice"><img class="choiceImg" src="./img/Loner.png" onclick="selectFaction(0)"><div class="imgDesc">LONERS</div></div>
-    <div class ="factionchoice"><img class="choiceImg" src="./img/Bandits.png" onclick="selectFaction(1)"><div class="imgDesc">BANDITS</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Loner.png" width="100px" height="100px" onclick="selectFaction(0)"><div class="imgDesc">LONERS</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Bandits.png" width="100px" height="120px" onclick="selectFaction(1)"><div class="imgDesc">BANDITS</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/ClearSky.png" width="100px" height="120px" onclick="selectFaction(2)"><div class="imgDesc">CLEAR SKY</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Duty.png" width="100px" height="100px" onclick="selectFaction(3)"><div class="imgDesc">DUTY</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Freedom.png" width="100px" height="100px" onclick="selectFaction(4)"><div class="imgDesc">FREEDOM</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Mercenary.png" width="100px" height="100px" onclick="selectFaction(5)"><div class="imgDesc">MERCENARY</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Army.png" width="100px" height="100px" onclick="selectFaction(6)"><div class="imgDesc">MILITARY</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Monolith.png" width="100px" height="100px" onclick="selectFaction(7)"><div class="imgDesc">MONOLITH</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Ecologists.png" width="100px" height="100px" onclick="selectFaction(8)"><div class="imgDesc">ECOLOGISTS</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/Renegade.png" width="90px" height="90px" onclick="selectFaction(9)"><div class="imgDesc">RENEGADE</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/SIN.png" width="85px" height="85px" onclick="selectFaction(10)"><div class="imgDesc">SIN</div></div>
+    <div class ="factionchoice"><img class="choiceImg" src="./img/UNISG.png" width="100px" height="100px" onclick="selectFaction(11)"><div class="imgDesc">UNISG</div></div>
     </div>
     <div id="genderChoice">
     <div class="generalBtn" onclick="chooseGender(0)">Masculine</div>
@@ -26,9 +36,16 @@ function checkFaction(faction) {
 function selectFaction(entry) {
     const fact = model.data.factions;
     const input = model.inputs;
+    const enemies = model.data.factions[entry].enemies;
+    let tempEnemyList = '';
+    let tempEnemyNum = 0;
+    for(let i = 0; i < enemies.length; i++) {
+        tempEnemyNum = enemies[i];
+        tempEnemyList += /*HTML*/ `<img class="enemyImg" src="${fact[tempEnemyNum].icon}" ${fact[tempEnemyNum].enicopx} alt="${fact[tempEnemyNum].name}"> `;
+    }
     input.hasUserPickedFaction = true;
     input.factionData = '';
-    input.factionData = /*HTML*/ `<div class = "selfactionBox"><img class="profileImg" src=${fact[entry].icon}><h4>${fact[entry].name}</h4><br><p4>${fact[entry].description}</p4><br><br><p5>Location(s): ${fact[entry].locations}</p5></div>`;
+    input.factionData = /*HTML*/ `<div class = "selfactionBox"><img class="profileImg" src=${fact[entry].icon}><h4>${fact[entry].name}</h4><br><p4>${fact[entry].description}</p4><br><br><p4>Location(s): ${fact[entry].locations}</p4><br><br><h4>Enemies:</h4> <div class="enemiesBox">${tempEnemyList}</div></div>`;
     console.log(input.factionData);
     updateView();
 }
