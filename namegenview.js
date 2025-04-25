52d6b1ffc1ef7145c85b877bd591828b07833f83
MGusto <felixbjorseth@outlook.com>
2025-04-25 13:42:53 +0200

    Further style edits to the namegen view

--- namegenview.js
+++ namegenview.js
@@ -1,5 +1,5 @@
 function nameGenView() {
-    return /*HTML*/ `<div class="contentBox">
+    return /*HTML*/ `<div class="contentBox" id="namegenView">
     <p>Name & Faction Generator feature is WIP. Most of the names are sourced from Grulag's <a class="hyperlink" href="https://www.moddb.com/mods/stalker-anomaly/addons/anomalys-name-update-for-stalkers-anus" target="_blank">"Anomaly's Name Update for Stalkers"<a>.</p>
     <h3>Choose the faction you want to play!</h3>
     <div id="generatorBox">
