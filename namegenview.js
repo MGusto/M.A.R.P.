5576870234ef82ff91b1a16323207889df0954c4
MGusto <felixbjorseth@outlook.com>
2025-04-25 13:21:22 +0200

    Refined visuals for faction selection in namegen view

--- namegenview.js
+++ namegenview.js
@@ -1,9 +1,8 @@
 function nameGenView() {
     return /*HTML*/ `<div class="contentBox">
     <p>Name & Faction Generator feature is WIP. Most of the names are sourced from Grulag's <a class="hyperlink" href="https://www.moddb.com/mods/stalker-anomaly/addons/anomalys-name-update-for-stalkers-anus" target="_blank">"Anomaly's Name Update for Stalkers"<a>.</p>
-    <div id="generatorBox">
     <h3>Choose the faction you want to play!</h3>
-    <div id="namefaction"></div>
+    <div id="generatorBox">
     <div id="factionbox">
     <div class ="factionchoice"><img class="choiceImg" src="./img/Loner.png" width="100px" height="100px" onclick="selectFaction(0)"><div class="imgDesc">LONERS</div></div>
     <div class ="factionchoice"><img class="choiceImg" src="./img/Bandits.png" width="100px" height="120px" onclick="selectFaction(1)"><div class="imgDesc">BANDITS</div></div>
@@ -18,12 +17,12 @@ function nameGenView() {
     <div class ="factionchoice"><img class="choiceImg" src="./img/SIN.png" width="85px" height="85px" onclick="selectFaction(10)"><div class="imgDesc">SIN</div></div>
     <div class ="factionchoice"><img class="choiceImg" src="./img/UNISG.png" width="100px" height="100px" onclick="selectFaction(11)"><div class="imgDesc">UNISG</div></div>
     </div>
-    <div id="genderChoice">
+    ${model.inputs.hasUserPickedFaction ? `${model.inputs.factionData}` : ''}</div></div>
+    ${model.inputs.hasUserPickedFaction ? `<div id="genderChoice" class="contentBox">
     <div class="generalBtn" onclick="chooseGender(0)">Masculine</div>
     <div class="generalBtn" onclick="chooseGender(1)">Feminine</div>
-    </div>
-    </div>
-    ${model.inputs.hasUserPickedFaction ? `${model.inputs.factionData}` : ''}`;
+    </div>` : ''}
+    `
     }
 
 function checkFaction(faction) {
